7f73c9bc07e42b0ec77e4ce0d0bcbe2538995254
violetta <lotsmanovavioletta@gmail.com>
2023-08-27 14:01:30 +0300

    update form

--- mailing/forms.py
+++ mailing/forms.py
@@ -3,19 +3,28 @@ from django import forms
 from mailing.models import Client, MailingSetting, Message
 
 
-class ClientForm(forms.ModelForm):
+class StyleFormMixin:
+    """Общий стиль форм"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class ClientForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Client
         fields = '__all__'
 
 
-class MailingSettingForm(forms.ModelForm):
+class MailingSettingForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = MailingSetting
         fields = '__all__'
 
 
-class MessageForm(forms.ModelForm):
+class MessageForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Message
         fields = '__all__'
