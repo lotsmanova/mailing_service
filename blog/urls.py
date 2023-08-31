d40199b3820ab0fe0da4bd7994d0f092b4fe7a3d
violetta <lotsmanovavioletta@gmail.com>
2023-08-31 18:49:09 +0300

    add CRUD app blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,16 @@
+from django.urls import path
+from django.views.decorators.cache import never_cache
+
 from blog.apps import BlogConfig
+from blog.views import BlogCreateView, BlogDetailView, BlogUpdateView, BlogDeleteView, BlogListView
 
 app_name = BlogConfig.name
-urlpatterns = []
+
+urlpatterns = [
+    path('blog_list/', BlogListView.as_view(), name='blog_list'),
+    path('blog_create/', never_cache(BlogCreateView.as_view()), name='blog_create'),
+    path('blog_detail/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('blog_update/<int:pk>/', BlogUpdateView.as_view(), name='blog_update'),
+    path('blog_delete/<int:pk>/', BlogDeleteView.as_view(), name='blog_delete'),
+
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,72 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from pytils.templatetags.pytils_translit import slugify
 
-# Create your views here.
+from blog.forms import BlogForm
+from blog.models import Blog
+
+
+class BlogCreateView(LoginRequiredMixin, CreateView):
+    """Контроллер создания"""
+
+    model = Blog
+    form_class = BlogForm
+    success_url = reverse_lazy('blog:blog_list')
+    login_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_net = form.save()
+            new_net.slug = slugify(new_net.head)
+            new_net.save()
+        return super().form_valid(form)
+
+
+class BlogListView(ListView):
+    """Контроллер общего просмотра"""
+
+    model = Blog
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_public=True)
+        return queryset
+
+
+class BlogDetailView(DetailView):
+    """Контроллер просмотра записи"""
+
+    model = Blog
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.count_views += 1
+        self.object.save()
+        return self.object
+
+
+class BlogUpdateView(LoginRequiredMixin, UpdateView):
+    """Контроллер редактирования"""
+    model = Blog
+    form_class = BlogForm
+    login_url = reverse_lazy('users:login')
+
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_net = form.save()
+            new_net.slug = slugify(new_net.head)
+            new_net.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blog:blog_detail', args=[self.kwargs.get('pk')])
+
+
+class BlogDeleteView(LoginRequiredMixin, DeleteView):
+    """Контроллер удаления"""
+    model = Blog
+    success_url = reverse_lazy('blog:blog_list')
+    login_url = reverse_lazy('users:login')
+
